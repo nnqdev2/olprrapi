b91d875b9803c428e064b67fc9120061e21348a9
Nga Quan <quan.nga@deq.state.or.us>
2018-08-06 10:54:13 -0700

    added staging and prod appsettings

--- OlprrApi/Startup.cs
+++ OlprrApi/Startup.cs
@@ -16,11 +16,15 @@ namespace OlprrApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
