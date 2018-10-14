556ec5f821882c6f4c0261e907001a6d545fa52d
Nga Quan <quan.nga@deq.state.or.us>
2018-10-13 19:41:04 -0700

    swagging lust controller

--- OlprrApi/Controllers/LustController.cs
+++ OlprrApi/Controllers/LustController.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OlprrApi.Models.Request;
 using OlprrApi.Models.Response;
 using OlprrApi.Services;
 
@@ -24,10 +26,10 @@ namespace OlprrApi.Controllers
 
         /// <summary>
         /// List of all site aliases for a lust id
+        /// (ApGetSiteAliasByLustIdData)
         /// </summary>
-        /// <remarks></remarks>
         /// <param name="lustId">LUST incident id</param>
-        [ProducesResponseType(200, Type = typeof(ApGetSiteAliasByLustId2))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ApGetSiteAliasByLustId2>))]
         [ProducesResponseType(400, Type = typeof(void))]
         [Route("lustid/{lustId}/sitealiases")]
         [HttpGet]
@@ -36,6 +38,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetSiteAliases(lustId));
         }
 
+        /// <summary>
+        /// Site Alias by site alias id
+        /// (ApGetSiteAliasBySiteNameAliasIdData)
+        /// </summary>
+        /// <param name="siteNameAliasId">LUST incident site alias id</param>
+        [ProducesResponseType(200, Type = typeof(ApGetSiteAliasByLustId2))]
         [Route("sitealias/{siteNameAliasId}")]
         [HttpGet]
         public async Task<IActionResult> GetSiteAlias(int siteNameAliasId)
@@ -43,6 +51,14 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetSiteAlias(siteNameAliasId));
         }
 
+        /// <summary>
+        /// Insert/Update site alias data for a lust incident
+        /// (ApInsUpdSiteAliasData)
+        /// </summary>
+        /// <param name="apInsUpdSiteAlias">Site alias data to insert or update</param>
+        [ProducesResponseType(201, Type = typeof(ApInsUpdSiteAlias))]
+        [ProducesResponseType(204, Type = typeof(void))]
+        [ProducesResponseType(400, Type = typeof(void))]
         [Route("sitealias")]
         [HttpPost]
         public async Task<IActionResult> InsUpdSiteAlias([FromBody] Models.Request.ApInsUpdSiteAlias apInsUpdSiteAlias)
@@ -56,6 +72,13 @@ namespace OlprrApi.Controllers
             return NoContent();
             //return Ok(await _olprrReviewService.InsUpdSiteAlias(apInsUpdSiteAlias));
         }
+
+        /// <summary>
+        /// Delete site alias data for a lust incident
+        /// (ApDltSiteNameAlias)
+        /// </summary>
+        /// <param name="siteNameAliasId">site alias id</param>
+        [ProducesResponseType(204, Type = typeof(void))]
         [Route("sitealias/{siteNameAliasId}")]
         [HttpDelete]
         public async Task<IActionResult> DeleteSiteAlias(int siteNameAliasId)
@@ -64,6 +87,18 @@ namespace OlprrApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// List of all contacts 
+        /// (ApGetPartyByFirstLastOrgNameData)
+        /// </summary>
+        /// <param name="fname">First name</param>
+        /// <param name="lname">Last name</param>
+        /// <param name="org">Organization</param>
+        /// <param name="sc">Sort column</param>
+        /// <param name="so">Sort order</param>
+        /// <param name="pn">Page number</param>
+        /// <param name="rpp">Rows per page</param>
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ContactsStats>))]
         [Route("contact")]
         [HttpGet]
         public async Task<IActionResult> GetContacts([FromQuery] string fname, [FromQuery] string lname, [FromQuery] string org, [FromQuery] int sc, [FromQuery] int so, [FromQuery] int pn, [FromQuery] int rpp)
@@ -71,6 +106,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetContacts(fname, lname, org, sc, so, pn, rpp));
         }
 
+        /// <summary>
+        /// GetCountyIdAndNameFromZP4Fips 
+        /// (ApGetCountyIdAndNameFromZP4Fips) 
+        /// </summary>
+        /// <param name="county">County code</param>
+        [ProducesResponseType(200, Type = typeof(ApGetCountyIdAndNameFromZP4Fips))]
         [Route("postalcounty/{county}")]
         [HttpGet]
         public async Task<IActionResult> GetCountyIdAndNameFromZP4Fips(int county)
@@ -78,12 +119,25 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetCountyIdAndNameFromZP4Fips(county));
         }
 
+        /// <summary>
+        /// Get LUST incident 
+        /// (ApGetIncidentByIdData) 
+        /// </summary>
+        /// <param name="lustId">LUST incident id</param>
+        [ProducesResponseType(200, Type = typeof(LustIncident))]
         [Route("incident/{lustId}")]
         [HttpGet]
         public async Task<IActionResult> GetIncidentByIdData(int lustId)
         {
             return Ok(await _lustService.GetIncidentByIdData(lustId));
         }
+
+        /// <summary>
+        /// Get list of current project managers
+        /// (ApGetCurrentProjMgr)
+        /// </summary>
+        /// <param name="lustId">LUST incident id</param>
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ProjectManager>))]
         [Route("pm/{lustId}")]
         [HttpGet]
         public async Task<IActionResult> GetCurrentProjMgr(int lustId)
@@ -91,6 +145,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetCurrentProjMgr(lustId));
         }
 
+        /// <summary>
+        /// Update LUST incident
+        /// (ApUpdIncidentData)
+        /// </summary>
+        /// <param name="apInsUpdIncidentData">LUST incident data</param>
+        [ProducesResponseType(200, Type = typeof(ApInsUpdIncidentDataResult))]
         [Route("incident")]
         [HttpPost]
         public async Task<IActionResult> UpdIncidentData([FromBody] Models.Request.ApInsUpdIncidentData apInsUpdIncidentData)
@@ -98,6 +158,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.InsUpdLustIncident(apInsUpdIncidentData));
         }
 
+        /// <summary>
+        /// Update LUST incident
+        /// (ApGetLogNumber)  
+        /// </summary>
+        /// <param name="lustId">LUST incident id</param>
+        /// [ProducesResponseType(200, Type = typeof(ApGetLogNumber))]
         [Route("lustid/{lustId}/lognumber")]
         [HttpGet]
         public async Task<IActionResult> GetLogNumber(int lustId)
@@ -105,6 +171,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetLogNumber(lustId));
         }
 
+        /// <summary>
+        /// Get a contact id for lust incident
+        /// (ApGetAffilByIdData) 
+        /// </summary>
+        /// <param name="affilId">LUST incident affiliate id</param>
+        /// [ProducesResponseType(200, Type = typeof(ApGetAffilById))]
         [Route("contact/{affilId}")]
         [HttpGet]
         public async Task<IActionResult> GetAffilById(int affilId)
@@ -112,14 +184,20 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetAffilById(affilId));
         }
 
-        [Route("contact/{affilId}")]
-        [HttpDelete]
-        public async Task<IActionResult> DeleteLustContactByAffilId(int affilId)
-        {
-            await _lustService.ApDltLustContactByAffilId(affilId);
-            return NoContent();
-        }
+        //[Route("contact/{affilId}")]
+        //[HttpDelete]
+        //public async Task<IActionResult> DeleteLustContactByAffilId(int affilId)
+        //{
+        //    await _lustService.ApDltLustContactByAffilId(affilId);
+        //    return NoContent();
+        //}
 
+        /// <summary>
+        /// Get all contacts for lust incident
+        /// (ApGetPartyGridByLustIdData) 
+        /// </summary>
+        /// <param name="lustId">LUST incident id</param>
+        /// [ProducesResponseType(200, Type = typeof(IEnumerable<ApGetAffilById>))]
         [Route("lustid/{lustId}/contact")]
         [HttpGet]
         public async Task<IActionResult> GetPartyGridByLustIdData(int lustId)
@@ -127,6 +205,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.GetPartyGridByLustIdData(lustId));
         }
 
+        /// <summary>
+        /// Insert/Update Contact for a LUST incident
+        /// (ApInsUpdLustAffilPartyData) 
+        /// </summary>
+        /// <param name="apInsUpdLustAffilPartyData">Contact data for a LUST incident</param>
+        /// [ProducesResponseType(200, Type = typeof(ApInsUpdLustAffilPartyDataResult))]
         [Route("lustid/{lustId}/contact")]
         [HttpPost]
         public async Task<IActionResult> InsUpdLustAffilPartyData([FromBody] Models.Request.ApInsUpdLustAffilPartyData apInsUpdLustAffilPartyData)
@@ -134,6 +218,12 @@ namespace OlprrApi.Controllers
             return Ok(await _lustService.InsUpdLustAffilPartyData(apInsUpdLustAffilPartyData));
         }
 
+        /// <summary>
+        /// Delete a lust incident
+        /// (ApDltIncidentByLustId)
+        /// </summary>
+        /// <param name="lustId">LUST incident id</param>
+        [ProducesResponseType(204, Type = typeof(void))]
         [Route("lustid/{lustId}")]
         [HttpDelete]
         public async Task<IActionResult> DeleteLustIncident(int lustId)
